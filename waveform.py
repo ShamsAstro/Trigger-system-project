3affb560501761876233b006e679615fe4996609
Shams Hassiki <moslemhassiki4@gmail.com>
2024-06-08 19:16:23 -0800

    This is test 1

--- waveform.py
+++ waveform.py
@@ -17,19 +17,31 @@ def burst_waveform( time, amp ):
     voltage = cosine_term * heaviside_term * exponent_term
     return voltage
 
+def triangle_wave(time,amp):
+    slope=1
 
+    triangle_at_t= time *slope
+    if triangle_at_t>amp:
+        triangle_at_t= 2*amp -time*slope
+    elif triangle_at_t<0:
+        triangle_at_t=0
+
+    return triangle_at_t
 """
 t_axis = np.array([])
 signal = np.array([])
 
-for i in range (100):
+for i in range (50):
     t_axis = np.append(t_axis,i)
-    signal = np.append(signal,burst_waveform(i))
+    signal = np.append(signal,triangle_wave(i, 25))
 
+print(t_axis[-5:], signal[-5:])
 plt.plot(t_axis, signal)
 plt.xlabel("Time (ns)")
 plt.ylabel("Voltage (mV)")
 plt.grid(True, lw=0.5)
 plt.title("random signal")
 plt.show()
+
+
 """
